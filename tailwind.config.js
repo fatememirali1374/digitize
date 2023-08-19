b851d65abe39571497c497eeeddd0760bb6fa062
fateme Mirali <fatememirali1374@gmail.com>
2023-08-19 15:38:52 +0330

    add plugin form and use from form-checkbox class

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+  ],
 }
 
